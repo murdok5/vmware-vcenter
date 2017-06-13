1377756395dfdc55882a426ec7f5f35ec7414bd7
murdok5 <mike.smith@puppet.com>
2017-06-13 10:18:39 -0700

    create PE 2017+ install class

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,11 @@
 class vcenter::install {
 
-  $packages_rhel = ['zlib-devel','libxslt-devel','patch','gcc'],
-  $packages_debian = ['zlib1g-dev','libxslt1-dev','build-essential'],
+  $packages_rhel = [ 'zlib-devel', 'libxslt-devel', 'patch', 'gcc']
+  $packages_debian = [ 'zlib1g-dev', 'libxslt1-dev', 'build-essential']
+
+  package { $packages_rhel
+    ensure => present,
+  }
 
   package { 'rbvmomi':
     ensure   => present,
