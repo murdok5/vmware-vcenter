afa881386dbb1ea40d12a8041f88a98f27888bef
murdok5 <mike.smith@puppet.com>
2017-06-13 10:07:37 -0700

    create PE 2017+ install class

---
+++ manifests/install.pp
@@ -0,0 +1,15 @@
+class vcenter::install {
+
+  $packages_rhel = ['zlib-devel','libxslt-devel','patch','gcc'],
+  $packages_debian = ['zlib1g-dev','libxslt1-dev','build-essential'],
+
+  package { 'rbvmomi':
+    ensure   => present,
+    provider => puppetserver_gem,
+  }
+
+  package { 'hocon':
+    ensure   => present,
+    provider => puppetserver_gem,
+  }
+}
